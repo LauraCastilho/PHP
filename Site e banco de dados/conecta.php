f0c9332cdf9a8b2c88a243805312cd8cdc14aa86
Laura de Andrade Castilho <54919378+LauraCastilho@users.noreply.github.com>
2020-06-08 11:34:12 -0300

    Adicionei o arquivo conecta.php

---
+++ Site e banco de dados/conecta.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "exercicio";
+    
+    $conexao = new MySQLi("$host", "$usuario", "$senha", "$banco");
+    $conexao->set_charset("utf8");
+    if ($conexao->connect_error) {
+        echo "ERRO DE CONEXÃO";
+    }
+    else {
+        echo "CONEXÃO BEM SUCEDIDA";
+    }
+?>
